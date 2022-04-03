031d61cc07987220d6182444bb86fd3473905d24
Alexander759 <86794464+Alexander759@users.noreply.github.com>
2022-04-03 19:04:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,72 @@
 // Classes
 
 class Teacher {
-    constructor(firstName, lastName, subjects) {
+    constructor(firstName, lastName, subjects, maxHours) {
         this.firstName = firstName
         this.lastName = lastName
         this.subjects = subjects
+        this.maxHours = maxHours
     }
 }
 
-class Subject {
-    constructor(name, numberOfHours, hoursToBeTogether, neededTeachers, difficulty) {
+class SubjectForClasses {
+    constructor(name, numberOfHours, hoursToBeTogether, neededTeachers, needComputerRoom) {
         this.name = name
         this.numberOfHours = numberOfHours
         this.hoursToBeTogether = hoursToBeTogether
         this.neededTeachers = neededTeachers
-        this.difficulty = difficulty
+        this.needComputerRoom = needComputerRoom
+        this.dificulty = 0;
+    }
+
+    AddDificulty() {
+        switch(this.name) {
+            case "Математика":
+                this.dificulty = 1;
+                break;
+            case "Информационни технологии":
+                this.dificulty = 1;
+                break;
+            case "Биология":
+                this.dificulty = 1;
+                break;
+            case "Физика":
+                this.dificulty = 1;
+                break;
+            case "Химия":
+                this.dificulty = 1;
+                break;
+            case "История":
+                this.dificulty = 1;
+                break;
+            case "Музика":
+                this.dificulty = 1;
+                break;
+            case "Изобразително изкуство":
+                this.dificulty = 1;
+                break;
+            case "Час на класа":
+                this.dificulty = 0;
+                break;
+            default:
+                this.dificulty = 1;
+        };
+    }
+
+}
+
+class SubjectForTeachers {
+    constructor(name, classes, needComputerRoom) {
+        this.name = name
+        this.classes= classes
+        this.needComputerRoom = needComputerRoom
     }
 }
 
-class Group
-{
-    constructor() {
-        this.numberOfGroups = 0
-        this.maxhours = 0
-        this.subjects = []
+class ClassInSchool {
+    constructor(name, subjects) {
+        this.name = name
+        this.subjects = subjects
     }
 
     AddSubjects(newSubject) {
@@ -32,11 +75,18 @@ class Group
 
 }
 
+class Classrooms {
+    constructor() {
+
+    }
+}
+
 //Containers
 
 const content = document.querySelector(".content")
 const classesInput = document.getElementById("number-of-unique-classes")
 const teachersInput = document.getElementById("number-of-teachers")
+const classroomsInput = document.getElementById("number-of-classrooms")
 const btnContinue = document.querySelector(".continue-button")
 
 //For creating forms for subjects
@@ -45,27 +95,34 @@ const formSubjectsClassesHolder = document.createElement("div")
 formSubjectsClassesHolder.classList.add("holder")
 formSubjectsClassesHolder.innerHTML = `
             <!-- Form for adding class' subjects -->
-            <div class="login-box">
+                <div class="login-box">
+                <p class="close-btn">&times</p>
+
                 <h2>Нов предмет</h2>
                 <form class>
                     <div class="user-box"> 
-                        <input list="possible-subjects" class="new-subject-add"/>
+                        <input list="possible-subjects" class="new-subject-add" id="class-subject-name"/>
                         <label>Предмет</label>
                     </div>
 
                     <div class="user-box">
-                        <input type="number" class="hours-the-subject"/>
+                        <input type="number" class="hours-the-subject" id="class-number-of-hours-of-subject"/>
                         <label>Брой часове за седмица</label>
                     </div>
 
                     <div class="user-box">
-                        <input type="number" class="teachers-for-the-subject"/>
+                        <input type="number" class="teachers-for-the-subject" id="class-needed-teachers"/>
                         <label>Колко учители са нужни на класа</label>
                     </div>
                     
                     <div class="user-box">
-                        <input list="yes-or-no" class="yes-or-no-input"/>
-                        <label>По възможност часовете да са заедно</label>
+                        <input type="number" id="class-how-many-classes-together"/>
+                        <label>По възможност колко от часовете да са заедно</label>
+                    </div>
+
+                    <div class="user-box">
+                        <input list="yes-or-no" id="class-need-computer-room"/>
+                        <label>Предметът нуждае ли се от компютърен кабинет</label>
                     </div>
 
                     <button class="add-subject-in-list-btn"> 
@@ -84,26 +141,24 @@ formSubjectsTeachersHolder.classList.add("holder")
 formSubjectsTeachersHolder.innerHTML +=`
     <!-- Form for adding teachers' subject -->
     <div class="login-box">
+        <p class="close-btn">&times</p>
+
         <h2>Нов предмет</h2>
         <form class>
             <div class="user-box"> 
-                <input list="possible-subjects" class="new-subject-add"/>
+                <input list="possible-subjects" class="new-subject-add" id="teacher-subject-name"/>
                 <label>Предмет</label>
-            </div>
+            </div>2
+
 
             <div class="user-box">
-                <input type="number" class="hours-the-subject"/>
-                <label>Брой часове за седмица</label>
-            </div>
-
-            <div class="user-box">
-                <input class="classes-wanted-teacher"/>
+                <input class="classes-wanted-teacher" id="teacher-wanted-classes"/>
                 <label>Желани класове (за предмета)</label>
             </div>
-            
+
             <div class="user-box">
-                <input <input type="number" class="hours-the-subject"/>
-                <label>Максимално брой часове за седмица (за предмета)</label>
+                    <input list="yes-or-no" id="teacher-need-computer-room"/>
+                    <label>Предметът нуждае ли се от компютърен кабинет</label>
             </div>
 
             <button class="add-subject-in-list-btn"> 
@@ -115,14 +170,23 @@ formSubjectsTeachersHolder.innerHTML +=`
         </form>
     </div>
 `
-console.log(formSubjectsClassesHolder.querySelector(".add-subject-in-list-btn"))
-console.log(formSubjectsTeachersHolder.querySelector(".add-subject-in-list-btn"))
 
 const buttonAddSubjectForClass = formSubjectsClassesHolder.querySelector(".add-subject-in-list-btn");
+const closeBtnFormClass = formSubjectsClassesHolder.querySelector(".close-btn")
+const subjectNameFormForClass = formSubjectsClassesHolder.querySelector("#class-subject-name")
+const numberOfHoursPerSubjectClassHolder = formSubjectsClassesHolder.querySelector("#class-number-of-hours-of-subject")
+const neededTeachersClassHolder = formSubjectsClassesHolder.querySelector("#class-needed-teachers")
+const classesTogether = formSubjectsClassesHolder.querySelector("#class-how-many-classes-together")
+const needComputerRoomClass = formSubjectsClassesHolder.querySelector("#class-need-computer-room")
+
 const buttonAddSubjectForTeacher = formSubjectsTeachersHolder.querySelector(".add-subject-in-list-btn");
+const closeBtnFormTeacher = formSubjectsTeachersHolder.querySelector(".close-btn")
+const subjectNameFormForTeacher = formSubjectsTeachersHolder.querySelector("#teacher-subject-name")
+const teacherWantedClasses = formSubjectsTeachersHolder.querySelector("#teacher-wanted-classes")
+const needComputerRoomTeacher = formSubjectsClassesHolder.querySelector("#teacher-need-computer-room")
+
 
 // Variables
-
 let activelist = null
 let btnsAddSubjects = null
 let btnAddSubjectToList = null
@@ -132,17 +196,29 @@ let btnAddSubjectToList = null
 function btnContinueIsClicked() {
     let numberOfClasses = classesInput.value
     let numberOfTeachers = teachersInput.value
+    let numberOfClassrooms = classroomsInput.value
 
-    let forms = ""
+    const datalist = document.createElement("datalist")
+    let valuesForDataList = ""
+
+    for(let i = 1; i < numberOfClassrooms; i++) {
+        valuesForDataList += `<option>${i}</option>`
+    }
+
+    datalist.innerHTML = valuesForDataList;
+    datalist.setAttribute("id", "possible-classrooms-names")
+    document.body.appendChild(datalist)
+
+    let formsAndBtn = ""
 
     for(let i = 0; i < numberOfClasses; i++)   {
-        forms += `
+        formsAndBtn += `
             <!-- Example of shablon subjects -->
             
             <form class="form-of-class">
                 <h2>Паралелка</h2>
 
-                <label>Име на класовете в паралелката</label>
+                <label>Имена на класовете в паралелката</label>
                 <input class="name-class"/>
 
                 <label>Предмети</label>
@@ -159,27 +235,30 @@ function btnContinueIsClicked() {
     }
 
     if(numberOfClasses % 3 == 2) {
-        forms += `
+        formsAndBtn += `
             <div></div>
         `
     } else if(numberOfClasses % 3 == 1) {
-        forms += `
+        formsAndBtn += `
             <div></div>
             <div></div>
         `
     }
 
     for(let i = 0; i < numberOfTeachers; i++)   {
-        forms += `
+        formsAndBtn += `
             <!-- Form for teacher -->
             <form class="form-of-teacher">
                 <h2>Учител</h2>
 
                 <label>Име</label>
-                <input class="name-teacher"/>
+                <input class="first-name-teacher"/>
 
                 <label>Фамилия</label>
                 <input class="last-name-teacher"/>
+
+                <label>Максимален брой часове за седмица</label>
+                <input <input type="number" class="hours-the-subject"/>
 
                 <label>Преподавани предмети</label>
                 <div>
@@ -195,12 +274,70 @@ function btnContinueIsClicked() {
         `
     }
 
-    content.innerHTML = forms;
+    if(numberOfClasses % 3 == 2) {
+        formsAndBtn += `
+            <div></div>
+        `
+    } else if(numberOfClasses % 3 == 1) {
+        formsAndBtn += `
+            <div></div>
+            <div></div>
+        `
+    }
+
+    for(let i = 0; i < numberOfClassrooms; i++)   {
+        formsAndBtn += `
+            <!-- Form for classroom -->
+            <form class="form-of-classroom">
+                <h2>Стая</h2>
+
+                <label>Име (номер)</label>
+                <input class="classroon-name" list="possible-classrooms-names"/>
+
+                <label>Клас</label>
+                <input/>
+
+                <label>Използва се с друг кабинет</label>
+                <input class="other-classroom-use" />
+
+            </form>
+        `
+    }
+
+    if(numberOfClasses % 3 == 2) {
+        formsAndBtn += `
+            <div></div>
+        `
+    } else if(numberOfClasses % 3 == 1) {
+        formsAndBtn += `
+            <div></div>
+            <div></div>
+        `
+    }
+
+    formsAndBtn += `
+            <div></div>
+        `
+
+    formsAndBtn += `
+            <button class="button-create">Създай</button>
+        `
+
+    formsAndBtn += `
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    `
+
+    content.innerHTML = formsAndBtn;
     content.classList.remove("first-content")
     content.classList.add("second-content")
 
-    btnsAddSubjectsClasses = content.querySelectorAll(".add-subject-btn-class")
-    btnsAddSubjectsClassesTeachers = content.querySelectorAll(".add-subject-btn-teacher")
+    const btnsAddSubjectsClasses = content.querySelectorAll(".add-subject-btn-class")
+    const btnsAddSubjectsClassesTeachers = content.querySelectorAll(".add-subject-btn-teacher")
+    const btnCreate = content.querySelector(".button-create")
 
     btnsAddSubjectsClasses.forEach(item => {
         item.addEventListener("click", addBtnIsClickedClasses)
@@ -210,6 +347,7 @@ function btnContinueIsClicked() {
          item.addEventListener("click", addBtnIsClickedTeachers)
     })
 
+    btnCreate.addEventListener("click", createtheSchedule)
 }
 
 function addBtnIsClickedClasses(target) {
@@ -226,44 +364,168 @@ function addBtnIsClickedTeachers(target) {
     content.appendChild(formSubjectsTeachersHolder)
 }
 
-function AddToClassList(item) {
+function AddToClassSubjectList(item) {
     item.preventDefault()
 
-    let subject = document.createElement("div")
+    const subject = document.createElement("div")
+    subject.setAttribute("class", "subject-box-in-lists")
 
-    const newSubjectInput = item.currentTarget.parentElement.querySelector(".new-subject-add")
 
-    if(newSubjectInput.value == "") {
-        alert("Не сте въвели текст")
-    } else {
-        subject.classList.add("new-subject-in-list")
-        subject.textContent += newSubjectInput.value
-        activelist.appendChild(subject)
-        newSubjectInput.value = ""
-        content.removeChild(formSubjectsClassesHolder);
+    if(subjectNameFormForClass.value == "") {
+        alert("Не сте избрали предмет")
+        return
     }
+    if(numberOfHoursPerSubjectClassHolder.value == ""){
+        alert("Не сте въвели брой часове")
+        return
+    } 
+    
+    if(neededTeachersClassHolder.value == "") {
+        alert("Не сте избрали брой учители")
+        return
+    }
+
+    if(classesTogether.value == "" ) {
+        alert("Не сте избрали колко от часовете да са заедно")
+        return
+    }
+
+    if(needComputerRoomClass.value == "Да" && needComputerRoomClass.value == "Не") {
+        alert("Не сте избрали дали предметът се нуждае от компютърна зала")
+        return
+    }
+
+    subject.classList.add("new-subject-in-list")
+    subject.innerHTML += `<p>${subjectNameFormForClass.value}</p>`
+    subject.info = new SubjectForClasses(subjectNameFormForClass.value, parseInt(numberOfHoursPerSubjectClassHolder.value), 
+        parseInt(classesTogether.value), parseInt(neededTeachersClassHolder.value, needComputerRoomClass.value == "Да"))
+    activelist.appendChild(subject)
+    content.removeChild(formSubjectsClassesHolder);
+    DefaultFormForClasses()
+    
 }
 
-function AddToTeacherList(item) {
+function AddToTeacherSubjectList(item) {
     item.preventDefault()
 
-    let subject = document.createElement("div")
+    const subject = document.createElement("div")
+    subject.setAttribute("class", "subject-box-in-lists")
 
     const newSubjectInput = item.currentTarget.parentElement.querySelector(".new-subject-add")
+    
+    if(subjectNameFormForTeacher.value == "") {
+        alert("Не сте избрали предмет")
+        return
+    } 
 
-    if(newSubjectInput.value == "") {
-        alert("Не сте въвели текст")
-    } else {
-        subject.classList.add("new-subject-in-list")
-        subject.textContent += newSubjectInput.value
-        activelist.appendChild(subject)
-        newSubjectInput.value = ""
-        content.removeChild(formSubjectsTeachersHolder);
+    if(needComputerRoomTeacher.value == "Да" && needComputerRoomTeacher.value == "Не") {
+        alert("Не сте избрали дали предметът се нуждае от компютърна зала")
+        return
     }
+
+    subject.classList.add("new-subject-in-list")
+    subject.textContent += newSubjectInput.value
+    subject.info = new SubjectForTeachers(subjectNameFormForTeacher.value, 
+        SeparateWords(teacherWantedClasses.value), needComputerRoomTeacher.value == "Да")
+    activelist.appendChild(subject)
+    content.removeChild(formSubjectsTeachersHolder);
+    DefaultFormForTeachers()
+    
+}
+
+function DefaultFormForClasses() {
+    subjectNameFormForClass.value = ""
+    numberOfHoursPerSubjectClassHolder.value = ""
+    neededTeachersClassHolder.value = ""
+    classesTogether.value = ""
+    needComputerRoomClass.value = ""
+}
+
+function DefaultFormForTeachers() {
+    subjectNameFormForTeacher.value = ""
+    teacherWantedClasses.value = ""
+    needComputerRoomTeacher.value = ""
+}
+
+function createtheSchedule() {
+    const AllCLasses = document.querySelectorAll(".form-of-class")
+    const AllTeachers = document.querySelectorAll(".form-of-teacher")
+    const AllClassrooms = document.querySelectorAll(".form-of-classroom")
+
+    let ClassesInSchool = []
+    let TeachersInSchool = []
+    // let ClassroomsInSchool = []
+
+    AllCLasses.forEach(function(classForm) {
+        let classes = SeparateWords(classForm.querySelector(".name-class").value)
+        let subjects = classForm.querySelectorAll(".subject-box-in-lists")
+        let subjectsInfo = []
+
+        for(let i = 0; i < subjects.length; i++) {
+            subjectsInfo.push(subjects[i].info)
+        }
+
+        for(let i = 0; i < classes.length; i++) {
+            ClassesInSchool.push(new ClassInSchool(classes[i], subjectsInfo))
+        }
+    })
+
+    AllTeachers.forEach(function(teacherForm) {
+        let firstName = teacherForm.querySelector(".first-name-teacher").value
+        let lastName = teacherForm.querySelector(".last-name-teacher").value
+        let maxHours= parseInt(teacherForm.querySelector(".hours-the-subject").value)
+        let subjects = teacherForm.querySelectorAll(".subject-box-in-lists")
+        
+        let subjectsInfo = []
+
+        for(let i = 0; i < subjects.length; i++) {
+            subjectsInfo.push(subjects[i].info)
+        }
+
+        TeachersInSchool.push(new Teacher(firstName, lastName, subjectsInfo, maxHours))
+
+    })
+
+    // AllClassrooms.forEach(function(classroomForm) {
+    //     let name = classroomForm.querySelector(".classroon-name").value
+    //     let otherRoomsUsedWithIt =  SeparateWords(classroomForm.querySelector(".other-classroom-use").value)
+    // })
+
+}
+
+function SeparateWords(words, seperators = [' ', ',']) {
+    let result = []
+    let newWord = ""
+
+    for(let i = 0; i < words.length; i++) {
+        if(seperators.includes(words[i])) {
+            if(newWord.length > 0) {
+                result.push(newWord)
+                newWord = ""
+            }
+        } else {
+            newWord += words[i]
+        }
+    }
+
+    if(newWord.length > 0) {
+        result.push(newWord)
+    }
+
+    return result
 }
 
 //Event listeners
 
 btnContinue.addEventListener("click", btnContinueIsClicked)
-buttonAddSubjectForClass.addEventListener("click", AddToClassList)
-buttonAddSubjectForTeacher.addEventListener("click", AddToTeacherList)
+buttonAddSubjectForClass.addEventListener("click", AddToClassSubjectList)
+buttonAddSubjectForTeacher.addEventListener("click", AddToTeacherSubjectList)
+
+closeBtnFormClass.addEventListener("click", function() { 
+    content.removeChild(formSubjectsClassesHolder)
+    DefaultFormForClasses()
+})
+closeBtnFormTeacher.addEventListener("click", function() { 
+    content.removeChild(formSubjectsTeachersHolder)
+    DefaultFormForTeachers()
+})
